0cbe56ccf08c1fdb00325aed5d3b2fd50e41f097
fellowbeard <virgovibe@gmail.com>
2022-04-19 16:11:17 -1000

    adds controller, adds secret auth

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,6 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   get "/news_api" => "news#index"
+
+  get "/auth/github/callback" => "github#authorize"
 end
